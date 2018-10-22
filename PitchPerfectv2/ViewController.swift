8693b1d364ceb932ee6ec007d8280acb3ed7a9e3
Tony Tong <tony.matthew.tong@gmail.com>
2018-10-22 11:53:54 -0400

    Initial Commit

---
+++ PitchPerfectv2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PitchPerfectv2
+//
+//  Created by Tony Tong on 2018-10-22.
+//  Copyright © 2018 TT. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
